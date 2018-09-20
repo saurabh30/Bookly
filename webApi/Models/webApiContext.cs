99f7a460e93d3ab6a46eb741223e82109ce24b86
saurabh30 <gupta.saurabh711@gmail.com>
2018-09-20 12:53:48 +0530

    created controllers and enabled  migration

---
+++ webApi/Models/webApiContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace webApi.Models
+{
+    public class webApiContext : DbContext
+    {
+        // You can add custom code to this file. Changes will not be overwritten.
+        // 
+        // If you want Entity Framework to drop and regenerate your database
+        // automatically whenever you change your model schema, please use data migrations.
+        // For more information refer to the documentation:
+        // http://msdn.microsoft.com/en-us/data/jj591621.aspx
+    
+        public webApiContext() : base("name=webApiContext")
+        {
+        }
+
+        public System.Data.Entity.DbSet<webApi.Models.Book> Books { get; set; }
+
+        public System.Data.Entity.DbSet<webApi.Models.Customer> Customers { get; set; }
+    }
+}
